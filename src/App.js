4bc1220c999ff1301e527bac62e92c60da9a30f4
Angela Bosilkovska <angela.bosilkovska@uklo.edu.mk>
2021-12-10 23:41:52 +0100

    State, New Expense Component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
+import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
 function App() {
   const expenses = [
-    { id: 'e1', title: "Car Insurance", amount: 296.95, date: new Date(2021, 2, 13) },
-    { id: 'e2',title: "New Tv", amount: 95.34, date: new Date(2020, 2, 28) },
-    { id: 'e3',title: "Toilet Paper", amount: 25.65, date: new Date(2021, 6, 8) },
-    { id: 'e4',title: "New Desk", amount: 450, date: new Date(2019, 5, 2) },
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amount: 296.95,
+      date: new Date(2021, 2, 13),
+    },
+    { id: "e2", title: "New Tv", amount: 95.34, date: new Date(2020, 2, 28) },
+    {
+      id: "e3",
+      title: "Toilet Paper",
+      amount: 25.65,
+      date: new Date(2021, 6, 8),
+    },
+    { id: "e4", title: "New Desk", amount: 450, date: new Date(2019, 5, 2) },
   ];
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <Expenses items={expenses}/>
+      <NewExpense />
+      <Expenses items={expenses} />
     </div>
   );
 }
